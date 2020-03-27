cd7666b4fc790a37381d9c030b0ddf911d96edcc
Leenus Darnal <leenus.darnal@gmail.com>
2020-03-27 13:50:04 +0530

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import Metronome from "./Metronome"
+
+const Destination = document.getElementById("root");
+
+ReactDOM.render(<Metronome/>,Destination);
