bda7bf6aae03c8bf6c3a587bf251dcc565f9e91b
leenusdarnal <leenus.darnal@gmail.com>
2020-12-18 13:06:05 +0530

    Updates

---
+++ precache-manifest.1fd510f8a10a7f06d8310d08c85eef1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6005fb969c6c7bc7868c035c8e1dbbc5",
+    "url": "/react-metronome/index.html"
+  },
+  {
+    "revision": "b8877fbf46c2ed57fdf0",
+    "url": "/react-metronome/static/css/main.5575e2c1.chunk.css"
+  },
+  {
+    "revision": "a38781231d0128e16f91",
+    "url": "/react-metronome/static/js/2.1f83920d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-metronome/static/js/2.1f83920d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8877fbf46c2ed57fdf0",
+    "url": "/react-metronome/static/js/main.22992f8d.chunk.js"
+  },
+  {
+    "revision": "44d01263d4ca52b8ffc6",
+    "url": "/react-metronome/static/js/runtime-main.693ebd43.js"
+  }
+]);
